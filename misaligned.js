860574a3f05f1815a4432a896192146cdbc9bfb4
Riswana <riswanafarween.mohamedmeeran@in.bosch.com>
2022-06-29 15:19:47 +0530

    passing the failed test

--- misaligned.js
+++ misaligned.js
@@ -5,12 +5,26 @@ function print_color_map() {
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     let i = 0;
     let j = 0;
+    let longestStringLength = getLongestString(majorColors);
     for (i = 0; i < majorColors.length; i++) {
         for (j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            if (i * 5 + j+1 < 10) {
+                console.log(`${i * 5 + j+1} | ${majorColors[i].padEnd(longestStringLength+1)}| ${minorColors[j]}`);
+            } else {
+                console.log(`${i * 5 + j+1}| ${majorColors[i].padEnd(longestStringLength)} | ${minorColors[j]}`);
+            } 
         }
     }
     return i * j;
+}
+function getLongestString(majorColors) {
+    let longestString = "";
+    for (i = 0; i < majorColors.length; i++) {
+        if (longestString.length <= majorColors[i].length) {
+            longestString = majorColors[i];
+        }
+    }
+    return longestString.length;
 }
 
 result = print_color_map();
